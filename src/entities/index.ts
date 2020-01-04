76b36b367c333dd6ade07dc78e2314a63a909e03
envy124 <pkgsail.dev@gmail.com>
2020-01-04 13:57:34 +0300

    add glob imports for providers & entities

---
+++ src/entities/index.ts
@@ -0,0 +1,6 @@
+export * from './instagram.campaign.entity';
+export * from './instagram.quest-type.entity';
+export * from './public.currency.entity';
+export * from './public.instagram.entity';
+export * from './public.server.entity';
+export * from './public.user.entity';
