1764446745a7a302cf3d7a2db08ad878ab335936
envy124 <pkgsail.dev@gmail.com>
2020-01-04 16:40:59 +0300

    fix nestjs typeorm config

--- src/modules/storage.module.ts
+++ src/modules/storage.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { InstagramStorageService } from '../providers';
 import {
   InstagramCampaignEntity,
@@ -10,9 +10,31 @@ import {
   UserEntity,
 } from '../entities';
 
+// nestjs cannot properly read ormconfig.js file, so db is not working
+const ormConfig: TypeOrmModuleOptions | undefined =
+  process.env.NODE_ENV === 'production'
+    ? undefined
+    : {
+        type: 'postgres',
+        host: 'acer',
+        port: 5432,
+        username: 'test',
+        password: 'test',
+        database: 'influence_cloud',
+        entities: [
+          InstagramCampaignEntity,
+          InstagramQuestTypeEntity,
+          CurrencyEntity,
+          InstagramEntity,
+          ServerEntity,
+          UserEntity,
+        ],
+        synchronize: false,
+        migrations: ['migrations'],
+      };
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot(ormConfig),
     TypeOrmModule.forFeature([
       InstagramCampaignEntity,
       InstagramQuestTypeEntity,
