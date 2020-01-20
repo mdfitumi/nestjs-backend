b0be0b28e6c472d2500507a71c7ffe367e5e785c
envy124 <pkgsail.dev@gmail.com>
2020-01-20 14:12:16 +0300

    fix ormconfig.js dotenv

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,13 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
 const SOURCE_PATH = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
+const config = dotenv.parse(
+  fs.readFileSync(`.${process.env.NODE_ENV || 'development'}.env`),
+);
 
 module.exports = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  url: config.DATABASE_URL,
   entities: [`${SOURCE_PATH}/**/**.entity{.ts,.js}`],
   synchronize: false,
   migrations: ['migrations/*.ts'],
